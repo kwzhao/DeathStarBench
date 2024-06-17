a1e9aa3023e180caf0670c1db0e6c0f14805f56f
Kevin Zhao <kzhao0986@gmail.com>
2024-06-17 14:21:02 -0700

    add the client interceptors back just in case

--- hotelReservation/tracing/interceptors.go
+++ hotelReservation/tracing/interceptors.go
@@ -63,3 +63,61 @@ func SizeTaggingUnaryServerInterceptor(
 	}
 	return resp, err
 }
+
+// ChainUnaryClientInterceptors chains multiple unary client interceptors into a single interceptor
+func ChainUnaryClientInterceptors(interceptors ...grpc.UnaryClientInterceptor) grpc.UnaryClientInterceptor {
+	return func(
+		ctx context.Context,
+		method string,
+		req, reply interface{},
+		cc *grpc.ClientConn,
+		invoker grpc.UnaryInvoker,
+		opts ...grpc.CallOption,
+	) error {
+		chainedInvoker := invoker
+		for i := len(interceptors) - 1; i >= 0; i-- {
+			interceptor := interceptors[i]
+			next := chainedInvoker
+			chainedInvoker = func(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, opts ...grpc.CallOption) error {
+				return interceptor(ctx, method, req, reply, cc, next, opts...)
+			}
+		}
+		return chainedInvoker(ctx, method, req, reply, cc, opts...)
+	}
+}
+
+// SizeTaggingUnaryClientInterceptor tags the OpenTracing span with the request and response sizes
+func SizeTaggingUnaryClientInterceptor(
+	ctx context.Context,
+	method string,
+	req, reply interface{},
+	cc *grpc.ClientConn,
+	invoker grpc.UnaryInvoker,
+	opts ...grpc.CallOption,
+) error {
+	span := opentracing.SpanFromContext(ctx)
+	if span != nil {
+		if reqProto, ok := req.(proto.Message); ok {
+			reqSize := proto.Size(reqProto)
+			span.SetTag("grpc.request.size", reqSize)
+			log.Info().Msgf("Request size for %s: %d bytes", method, reqSize)
+		} else {
+			log.Warn().Msgf("Request for method %s is not a proto.Message", method)
+			span.SetTag("grpc.request.size", "unknown")
+		}
+	}
+	err := invoker(ctx, method, req, reply, cc, opts...)
+	if span != nil {
+		if err == nil {
+			if replyProto, ok := reply.(proto.Message); ok {
+				respSize := proto.Size(replyProto)
+				span.SetTag("grpc.response.size", respSize)
+				log.Info().Msgf("Response size for %s: %d bytes", method, respSize)
+			} else {
+				log.Warn().Msgf("Response for method %s is not a proto.Message", method)
+				span.SetTag("grpc.response.size", "unknown")
+			}
+		}
+	}
+	return err
+}
